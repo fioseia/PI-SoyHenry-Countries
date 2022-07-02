d66fd1b5abf76c72575ef1b7b51bbad054766a56
fioseia <fio.seia@hotmail.com>
2022-07-01 21:11:21 -0300

    frontend deploy

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
+import dotenv from 'dotenv';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+
+
+dotenv.config();
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
 	<Provider store={store}>
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 export const GET_ALL_COUNTRIES = 'GET_ALL_COUNTRIES';
 export const GET_FILTERED_COUNTRIES = 'GET_FILTERED_COUNTRIES';
 export const GET_CATEGORIES = 'GET_CATEGORIES';
@@ -6,13 +8,12 @@ export const SET_ORDER_NAME = 'SET_ORDER_NAME';
 export const GET_COUNTRY_DETAILS = 'GET_COUNTRY_DETAILS';
 export const POST_ACTIVITY = 'POST_ACTIVITY';
 
-const PATH = 'http://localhost:3001';
+
 
 export const getAllCountries = () => {
 	return function (dispatch) {
-		fetch(`${PATH}/countries`)
-			.then((response) => response.json())
-			.then((data) => dispatch({ type: GET_ALL_COUNTRIES, payload: data }))
+	return axios.get('/countries')
+			.then((data) => dispatch({ type: GET_ALL_COUNTRIES, payload: data.data }))
 			.catch((error) => console.log(error));
 	};
 };
@@ -25,24 +26,22 @@ export const getFilteredCountries = ({
 }) => {
   console.log(continent)
 	return function (dispatch) {
-		fetch(
-			`${PATH}/countries?name=${encodeURI(name)}&continent=${encodeURI(
+		return axios.get(
+			`/countries?name=${encodeURI(name)}&continent=${encodeURI(
 				continent
 			)}&category=${encodeURI(category)}&subcategory=${encodeURI(
 				subcategory
 			)}`
 		)
-			.then((response) => response.json())
-			.then((data) => dispatch({ type: GET_FILTERED_COUNTRIES, payload: data }))
+			.then((data) => dispatch({ type: GET_FILTERED_COUNTRIES, payload: data.data }))
 			.catch((error) => console.log(error));
 	};
 };
 
 export const getCategories = () => {
 	return function (dispatch) {
-		fetch(`${PATH}/activities`)
-			.then((response) => response.json())
-			.then((data) => dispatch({ type: GET_CATEGORIES, payload: data }))
+		return axios.get(`/activities`)
+			.then((data) => dispatch({ type: GET_CATEGORIES, payload: data.data }))
 			.catch((error) => console.log(error));
 	};
 };
@@ -61,9 +60,8 @@ export const sortCountriesName = (order) => {
 
 export const getCountryDetails = (id) => {
   return function (dispatch) {
-    fetch(`${PATH}/countries/${id}`)
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: GET_COUNTRY_DETAILS, payload: data }))
+    return axios.get(`/countries/${id}`)
+      .then((data) => dispatch({ type: GET_COUNTRY_DETAILS, payload: data.data }))
       .catch((error) => console.log(error));
   };
 }
